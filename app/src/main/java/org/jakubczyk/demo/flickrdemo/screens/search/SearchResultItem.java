7495ddaae657866ca99d167923a28ae263279e4a
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-07-30 22:18:53 +0200

    Use databinding and picasso to load images

--- app/src/main/java/org/jakubczyk/demo/flickrdemo/screens/search/SearchResultItem.java
+++ app/src/main/java/org/jakubczyk/demo/flickrdemo/screens/search/SearchResultItem.java
@@ -8,14 +8,28 @@ import org.jakubczyk.demo.flickrdemo.databinding.SearchResultItemBinding;
 class SearchResultItem extends RecyclerView.ViewHolder {
 
     private final SearchResultItemBinding binding;
+    private final SearchItemViewModel searchItemViewModel;
 
     public SearchResultItem(SearchResultItemBinding binding) {
         super(binding.getRoot());
 
         this.binding = binding;
+        searchItemViewModel = new SearchItemViewModel();
+        binding.setViewModel(searchItemViewModel);
     }
 
     void bind(Photo photo) {
+        // url template
+        // http://farm{farm}.static.flickr.com/{server}/{id}_{secret}.jpg
+        String url = String.format(
+                "http://farm%s.static.flickr.com/%s/%s_%s.jpg",
+                photo.farm,
+                photo.server,
+                photo.id,
+                photo.secret
+        );
 
+
+        searchItemViewModel.setUrl(url);
     }
 }
