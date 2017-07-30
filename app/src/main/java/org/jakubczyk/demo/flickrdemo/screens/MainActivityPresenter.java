66f6037eef3b16d6abef5e3776112564c0cd95fe
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-07-30 21:36:16 +0200

    More lambdas

--- app/src/main/java/org/jakubczyk/demo/flickrdemo/screens/MainActivityPresenter.java
+++ app/src/main/java/org/jakubczyk/demo/flickrdemo/screens/MainActivityPresenter.java
@@ -1,18 +1,12 @@
 package org.jakubczyk.demo.flickrdemo.screens;
 
 
-import org.jakubczyk.demo.flickrdemo.data.api.json.Photo;
-import org.jakubczyk.demo.flickrdemo.data.api.json.SearchResponse;
 import org.jakubczyk.demo.flickrdemo.data.repository.FlickrRepository;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Scheduler;
-import rx.Subscriber;
-import rx.functions.Action1;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivityPresenter implements MainActivityContract.Presenter {
@@ -41,41 +35,12 @@ public class MainActivityPresenter implements MainActivityContract.Presenter {
         charSequenceObservable
                 // don't flood with requests
                 .debounce(3, TimeUnit.SECONDS, scheduler)
-                .filter(charSequence -> charSequence.length() > 0)
-                .doOnNext(new Action1<CharSequence>() {
-                    @Override
-                    public void call(CharSequence charSequence) {
-
-                    }
-                })
-                .flatMap(new Func1<CharSequence, Observable<SearchResponse>>() {
-                    @Override
-                    public Observable<SearchResponse> call(CharSequence charSequence) {
-                        return flickrRepository.searchFlickr(charSequence.toString());
-                    }
-                })
-                .map(new Func1<SearchResponse, List<Photo>>() {
-                    @Override
-                    public List<Photo> call(SearchResponse searchResponse) {
-                        return searchResponse.photos.photoList;
-                    }
-                })
-                .subscribe(new Subscriber<List<Photo>>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(List<Photo> photoList) {
-                        view.addPhotos(photoList);
-                    }
-                });
+                .filter(textToSearch -> textToSearch.length() > 0)
+                .flatMap(textToSearch -> flickrRepository.searchFlickr(textToSearch.toString()))
+                .map(searchResponse -> searchResponse.photos.photoList)
+                .subscribe(
+                        photoList -> view.addPhotos(photoList)
+                );
     }
 
 }
