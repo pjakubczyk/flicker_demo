6964f38a01bd6ec307c3e9233a033ae9cd3901aa
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-07-31 20:20:39 +0200

    Adding test to scroll results few times

--- app/src/androidTest/java/org/jakubczyk/demo/flickrdemo/SearchInstrumentedTest.java
+++ app/src/androidTest/java/org/jakubczyk/demo/flickrdemo/SearchInstrumentedTest.java
@@ -3,6 +3,8 @@ package org.jakubczyk.demo.flickrdemo;
 import android.os.SystemClock;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +20,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
@@ -40,11 +43,27 @@ public class SearchInstrumentedTest {
         onView(isAssignableFrom(AutoCompleteTextView.class)).perform(typeText("kitten"));
         onView(isAssignableFrom(AutoCompleteTextView.class)).perform(closeSoftKeyboard());
 
-        SystemClock.sleep(3500);
+        SystemClock.sleep(1500);
+
+        // NOTE 21, 41, 61, 81 - the one is for the spinner item because it's part of the recycler view
 
         // then: Check that the text was changed.
         onView(withId(R.id.search_result_list))
-                .check(new RecyclerViewItemCountAssertion(20));
+                .check(new RecyclerViewItemCountAssertion(21));
+
+        onView(ViewMatchers.withId(R.id.search_result_list)).perform(ViewActions.swipeUp());
+
+        // wait for next page
+        SystemClock.sleep(1500);
+        onView(withId(R.id.search_result_list))
+                .check(new RecyclerViewItemCountAssertion(41));
+
+        onView(ViewMatchers.withId(R.id.search_result_list)).perform(ViewActions.swipeUp());
+
+        // wait for next page
+        SystemClock.sleep(1500);
+        onView(withId(R.id.search_result_list))
+                .check(new RecyclerViewItemCountAssertion(61));
     }
 
 
